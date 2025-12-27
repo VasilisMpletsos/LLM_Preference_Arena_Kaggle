218af68dc2a34ac620f566e58f1594828239ff8e
vmpletsos <msa17020@gmail.com>
2025-12-27 14:45:05 +0200

    Work for dataset creation

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .clean import remove_extra_brackets
+from .prompts import CLASSIFICATION_PROMPT
 
-__all__ = ["remove_extra_brackets"]
+__all__ = ["remove_extra_brackets", "CLASSIFICATION_PROMPT"]
---
+++ src/utils/prompts.py
@@ -0,0 +1,13 @@
+CLASSIFICATION_PROMPT = """
+You are a helpfful expert that can distinguish the User's preferred response among 2 options.
+The available options are Winner A B or Tie.
+
+The Prompt is:
+{prompt}
+
+Response A:
+{response_a}
+
+Response B:
+{response_b}
+"""
